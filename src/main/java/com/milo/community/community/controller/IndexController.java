f33532df612c95f58d4932068f72b3b0596f907e
milo <bingyangwu.cn@gmail.com>
2020-03-18 21:59:27 +0800

    导航栏

---
+++ src/main/java/com/milo/community/community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.milo.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author milo
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+}
