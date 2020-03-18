eb02ece88917156a1c4f82a2e8cc24cd676a10b5
milo <bingyangwu.cn@gmail.com>
2020-03-18 21:11:53 +0800

    init

---
+++ src/main/java/com/milo/community/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package com.milo.community.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author milo
+ * @date 2020/3/18
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
