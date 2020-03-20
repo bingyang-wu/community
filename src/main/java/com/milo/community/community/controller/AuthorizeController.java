d52aec99e2eb501db5bfc288d3665b74c53d089b
milo <bingyang_w@sina.com>
2020-03-20 09:46:29 +0800

    github登录请求和回调

---
+++ src/main/java/com/milo/community/community/controller/AuthorizeController.java
@@ -0,0 +1,22 @@
+package com.milo.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 授权
+ *
+ * @author milo
+ * @date 2020/3/19
+ */
+@Controller
+public class AuthorizeController {
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        
+        return "index";
+    }
+}
