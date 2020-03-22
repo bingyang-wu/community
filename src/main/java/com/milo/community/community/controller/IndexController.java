566b86a13dd04a053b0d5f03fe4151fda96bfc9e
milo <bingyang_w@sina.com>
2020-03-22 15:33:24 +0800

    cookie保持用户持久化登录

--- src/main/java/com/milo/community/community/controller/IndexController.java
+++ src/main/java/com/milo/community/community/controller/IndexController.java
@@ -1,16 +1,42 @@
 package com.milo.community.community.controller;
 
+import com.milo.community.community.mapper.UserMapper;
+import com.milo.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author milo
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+
+                    break;
+                }
+            }
+        }
+
         return "index";
     }
 }
